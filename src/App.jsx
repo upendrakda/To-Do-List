8c003729cbd3307fbca6bbe359fdd14b335bd0af
upendrakda <upendrakda123@gmail.com>
2026-01-15 20:12:16 +0545

    feat: add controlled input state for todo entry and improved UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
+
 function App() {
+  const list=[];
+  const [text, setText] = useState('');
+  const [task, setTask] = useState('');
+
   return (
     <div className='container'>
       <h1>Your To Do</h1>
-      <input type="text" className='input' placeholder='Add new task' />
-      <button className='add-btn'>+</button>
+      <input 
+        type="text" 
+        className='input' 
+        placeholder='Add new task'
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <button 
+        className='add-btn'
+        onClick={() => setText('')}
+      >+</button>
+      
+      <div className='wrapper'>
+        <input type="checkbox" id='test' className="checkbox" />
+        <label htmlFor="test" className='taskText'>Hello World!!</label>
+        <button className='delete'>&#10006;</button>
+      </div>
+
     </div>
   );
 }
