fe97496c08b3b5f9284b10804bf2e6d1cff818b8
upendrakda <upendrakda123@gmail.com>
2026-01-17 10:13:25 +0545

    feat: prevent adding empty todo items and add task on key press Enter

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,21 @@ function App() {
   const [text, setText] = useState('');
   const [task, setTask] = useState([]);
 
+  function addTask(){
+    if (!text.trim()) return;
+
+    setText('');
+    setTask([{
+      id: Math.random().toString(36).slice(2, 9),
+      text: text}, ...task]);
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  };
+
   return (
     <div className='container'>
       <h1>Your To Do</h1>
@@ -15,20 +30,16 @@ function App() {
         placeholder='Add new task'
         value={text}
         onChange={(e) => setText(e.target.value)}
+        onKeyDown={handleKeyDown}
       />
 
       <button 
         className='add-btn'
-        onClick={() => {
-          setText('');
-          setTask([{
-            id: task.length + 1,
-            text: text}, ...task]);
-        }}
+        onClick={addTask}
       >+</button>
       
       {task.map(item => (
-      <div className='wrapper'>
+      <div className='wrapper' key={item.id}>
         <input type="checkbox" id={item.id} className="checkbox" />
         <label htmlFor={item.id} className='taskText'>{item.text}</label>
         <button className='delete'>&#10006;</button>
