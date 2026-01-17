8a8582a32a009434352e2a84cb6d90e2d43b1a22
upendrakda <upendrakda123@gmail.com>
2026-01-17 13:22:54 +0545

    feat: add task deletion and task counter

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,24 @@ import './App.css';
 function App() {
   const [text, setText] = useState('');
   const [task, setTask] = useState([]);
+  const [count, setCount] = useState(0);
 
   function addTask(){
-    if (!text.trim()) return;
+    if (!text.trim()){
+      setText('');
+      return;
+    }
 
-    setText('');
     setTask([{
       id: Math.random().toString(36).slice(2, 9),
       text: text}, ...task]);
+    setText('');
+    setCount(count+1);
+  }
+
+  function deleteTask(id){
+    setTask(task.filter(t => t.id !== id));
+    setCount(count-1);
   }
 
   const handleKeyDown = (event) => {
@@ -23,29 +33,38 @@ function App() {
 
   return (
     <div className='container'>
-      <h1>Your To Do</h1>
-      <input 
-        type="text" 
-        className='input' 
-        placeholder='Add new task'
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        onKeyDown={handleKeyDown}
-      />
+      <div className="content">
+        <h1>Your To Do</h1>
+        <input 
+          type="text" 
+          className='input' 
+          placeholder='Add new task'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
 
-      <button 
-        className='add-btn'
-        onClick={addTask}
-      >+</button>
-      
-      {task.map(item => (
-      <div className='wrapper' key={item.id}>
-        <input type="checkbox" id={item.id} className="checkbox" />
-        <label htmlFor={item.id} className='taskText'>{item.text}</label>
-        <button className='delete'>&#10006;</button>
+        <button 
+          className='add-btn'
+          onClick={addTask}
+        >+</button>
+
+        {task.map(item => (
+        <div className='wrapper' key={item.id}>
+          <input type="checkbox" id={item.id} className="checkbox" />
+          <label htmlFor={item.id} className='taskText'>{item.text}</label>
+          <button 
+            className='delete'
+            onClick={() => deleteTask(item.id)}
+            >&#10006;</button>
+        </div>
+        ))}
       </div>
-      ))}
 
+      <footer>
+        <h3>Your remaining todos: {count}</h3>
+        <p><q>What you do today can improve all your tomorrows</q> — Ralph Marston</p>
+      </footer>
     </div>
   );
 }
